ce03aea4adf5a4152ee121737e2e1d5f1bd8c6a6
Finnneon <finnneon1@gmail.com>
2023-08-19 12:03:15 -0400

    Only scan mp3 files
    Don't scan other files

--- main.go
+++ main.go
@@ -5,7 +5,18 @@ import (
 	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
+
+// isMP3 checks if a file is an MP3. Ignores other file formats not supported by Safari
+func isMP3(path string) bool {
+	parts := strings.Split(path, ".")
+	if len(parts) == 1 {
+		return false
+	}
+	extension := parts[len(parts)-1]
+	return extension == "mp3"
+}
 
 func main() {
 	root := "/home/finnneon/Music"
@@ -15,6 +26,9 @@ func main() {
 		}
 		if d.IsDir() {
 			return nil // skip over directories
+		}
+		if !isMP3(path) {
+			return nil
 		}
 		fmt.Println(path)
 		return nil
